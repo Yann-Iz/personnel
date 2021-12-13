a5eaa2c13dbbb7dcfac69cfabe0d003a37b351ea
Yann-Iz <83369372+Yann-Iz@users.noreply.github.com>
2021-12-13 01:38:41 +0100

    testLigue

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -12,15 +12,56 @@ class testLigue
 	@Test
 	void createLigue() throws SauvegardeImpossible
 	{
-		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
-		assertEquals("Fléchettes", ligue.getNom());
+		Ligue ligue = gestionPersonnel.addLigue("Basketball");
+		assertEquals("Basketball", ligue.getNom());
 	}
-
+	
+	@Test
+	void modifLigue() throws SauvegardeImpossible
+	{
+		Ligue ligue=gestionPersonnel.addLigue("Basketball");
+		ligue.setNom("Basket");
+		assertEquals("Basket",ligue.getNom());
+	}
+	
+	@Test
+	void deleteLigue() throws SauvegardeImpossible
+	{
+		Ligue ligue =gestionPersonnel.addLigue("Basketball");
+		ligue.remove();
+		assertEquals(false, gestionPersonnel.getLigues().contains(ligue));
+	}
+	
 	@Test
 	void addEmploye() throws SauvegardeImpossible
 	{
-		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Ligue ligue = gestionPersonnel.addLigue("Basketball");
 		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
 		assertEquals(employe, ligue.getEmployes().first());
+	}
+	
+	@Test
+	void modifEmploye() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Basketball");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty");
+		Employe employe2 = ligue.addEmploye("Bouchard2","Gérard2","g.bouchard2@gmail.com", "azerty2");
+		employe.setNom("Bouchard2");
+		employe.setPrenom("Gérard2");
+		employe.setMail("g.bouchard2@gmail.com");
+		employe.setPassword("azerty2");
+		assertEquals(employe2.getNom(),employe.getNom());
+		assertEquals(employe2.getPrenom(),employe.getPrenom());	
+		assertEquals(employe2.getMail(),employe.getMail());	
+	}
+	
+	@Test
+	void deleteEmploye() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Basketball");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty");
+		employe.remove();
+		assertEquals(false,ligue.getEmployes().contains(employe));
+		
 	}
 }
